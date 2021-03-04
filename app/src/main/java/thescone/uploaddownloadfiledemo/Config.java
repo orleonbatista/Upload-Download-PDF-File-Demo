cbbdeea3646a2720fed2dc41a5391e73e27f13fa
orleon batista <orleonbatista@gmail.com>
2021-03-03 22:24:22 -0300

    Update Config.java

--- app/src/main/java/thescone/uploaddownloadfiledemo/Config.java
+++ app/src/main/java/thescone/uploaddownloadfiledemo/Config.java
@@ -1,11 +1,8 @@
 package thescone.uploaddownloadfiledemo;
 
-/**
- * Created by Habibah on 03-Mar-17.
- */
 public class Config {
 
-    public static final String WEB = "http://YOUR-DOMAIN-HERE";
+    public static final String WEB = "http://localhost";
     public static final String UPLOAD_URL = WEB+"/upload.php";
     public static final String PDF_FETCH_URL = WEB+"/getPDF.php";
 }
